2501fc54e781f14a3253768ae7f2cfbb32e60c5e
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2025-09-10 00:00:47 -0500

    feat: scaffold authentication

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/rs/zerolog/log"
 )
@@ -18,14 +21,40 @@ type ShoppingList struct {
 var PORT = 8888
 var allData []ShoppingList = []ShoppingList{}
 
+type User struct {
+	Role     string
+	Username string
+	Password string
+}
+
+type Session struct {
+	Expires  time.Time
+	Username string
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var sessions = map[string]*Session{}
+
+var allUsers = map[string]*User{
+	"admin": {Role: "admin", Username: "admin", Password: "password"},
+	"user":  {Role: "user", Username: "user", Password: "password"},
+}
+
 func main() {
-	http.HandleFunc("POST /v1/lists", handleCreateList)
-	http.HandleFunc("GET /v1/lists", handleGetLists)
-	http.HandleFunc("DELETE /v1/lists/{id}", handleDeleteList)
-	http.HandleFunc("PUT /v1/lists/{id}", handleUpdateList)
-	http.HandleFunc("PATCH /v1/lists/{id}", handlePatchList)
-	http.HandleFunc("GET /v1/lists/{id}", handleGetList)
-	http.HandleFunc("POST /v1/lists/{id}/push", handleListPush)
+
+	http.HandleFunc("POST /v1/lists", adminRequired(handleCreateList))
+	http.HandleFunc("GET /v1/lists", authRequired(handleGetLists))
+	http.HandleFunc("PUT /v1/lists/{id}", adminRequired(handleUpdateList))
+	http.HandleFunc("DELETE /v1/lists/{id}", adminRequired(handleDeleteList))
+	http.HandleFunc("PATCH /v1/lists/{id}", adminRequired(handlePatchList))
+	http.HandleFunc("GET /v1/lists/{id}", authRequired(handleGetList))
+	http.HandleFunc("POST /v1/lists/{id}/push", adminRequired(handleListPush))
+
+	http.HandleFunc("POST /v1/login", handleLogin)
 
 	log.Info().Msgf("> Server running on http://localhost:%d\n", PORT)
 	// this blocks the thread so code after this line will not run
@@ -220,4 +249,83 @@ func handleListPush(w http.ResponseWriter, r *http.Request) {
 
 	http.Error(w, "list not found", http.StatusNotFound)
 
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	var data LoginRequest
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	user := allUsers[data.Username]
+	if user != nil && user.Password == data.Password {
+		token := strconv.Itoa(rand.Intn(100000000000))
+		sessions[token] = &Session{
+			Expires:  time.Now().Add(7 * 24 * time.Hour),
+			Username: user.Username,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		err := json.NewEncoder(w).Encode(map[string]string{"token": token})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		return
+	}
+
+	http.Error(w, "invalid credentials", http.StatusUnauthorized)
+}
+
+func authRequired(next http.HandlerFunc) http.HandlerFunc {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		if !strings.HasPrefix(token, "Bearer") {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		token = token[7:]
+
+		if sessions[token] == nil {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if sessions[token].Expires.Before(time.Now()) {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		userSession := sessions[token]
+		user := allUsers[userSession.Username]
+
+		if user == nil {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		next(w, r)
+	}
+
+	return fn
+}
+
+func adminRequired(next http.HandlerFunc) http.HandlerFunc {
+	return authRequired(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		token = token[7:]
+		user := allUsers[sessions[token].Username]
+
+		if user.Role != "admin" {
+			http.Error(w, "forbidden", http.StatusForbidden)
+			return
+		}
+
+		next(w, r)
+	})
 }
